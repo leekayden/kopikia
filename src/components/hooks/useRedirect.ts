555a52d693b44eac858392f14dcd02a320436408
Kayden Lee <kayden@programmer.net>
2023-04-02 12:53:46 +0800

    add useRedirect react hook

---
+++ src/components/hooks/useRedirect.ts
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+const useRedirect = (to: string, seconds: number = 5) => {
+  const [countdown, setCountdown] = useState(seconds);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCountdown((prevCountdown) => prevCountdown - 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+    if (countdown === 0) {
+      window.location.href = to;
+    }
+  }, [countdown, to]);
+
+  return { countdown };
+};
+
+export default useRedirect;
