56cb5274e8735c7c8051c525d93d207e204f8730
Kayden Lee <kayden@programmer.net>
2023-04-02 12:53:55 +0800

    add img btns with no image

--- src/components/common/Home.tsx
+++ src/components/common/Home.tsx
@@ -1,9 +1,34 @@
 import NavBar from "./NavBar";
+import ImgBtn from "../components/ImgBtn";
+
+let images = [
+  {
+    url: "",
+    title: "Take Orders",
+    width: "25%",
+  },
+  {
+    url: "",
+    title: "Use previous orders",
+    width: "25%",
+  },
+  {
+    url: "",
+    title: "Find good deals",
+    width: "25%",
+  },
+  {
+    url: "",
+    title: "Take orders with friends",
+    width: "25%",
+  },
+];
 
 function Home() {
   return (
     <div>
       <NavBar />
+      <ImgBtn imgLs={images} />
     </div>
   );
 }
