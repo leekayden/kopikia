c29901030e11ff07e43ba477d3b7bfdbc1d37982
leekayden <kayden@programmer.net>
2023-04-03 19:43:04 +0800

    add grid

--- src/components/common/Order.tsx
+++ src/components/common/Order.tsx
@@ -1,40 +1,21 @@
 import NavBar from "./NavBar";
 import TakeOrder from "./TakeOrder";
-import { styled } from "@mui/material/styles";
-import Box from "@mui/material/Box";
-import Paper from "@mui/material/Paper";
-import Grid from "@mui/material/Grid";
-import Button from '@mui/material/Button';
-
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: "center",
-  color: theme.palette.text.secondary,
-}));
+import { Grid } from "@mui/material";
 
 function Order() {
   return (
     <div>
       <NavBar />
       <br />
-      <Box sx={{ flexGrow: 1 }}>
-        <Grid container spacing={2}>
-          <Grid item xs={8}>
-            <Item><TakeOrder /></Item>
-          </Grid>
-          <Grid item xs={4}>
-            <Item>xs=4</Item>
-          </Grid>
-          <Grid item xs={4}>
-            <Item>xs=4</Item>
-          </Grid>
-          <Grid item xs={8}>
-            <Item>xs=8</Item>
-          </Grid>
-        </Grid>
-      </Box>
+      <Grid
+        container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <TakeOrder />
+      </Grid>
     </div>
   );
 }
