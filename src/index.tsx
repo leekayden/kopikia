6afb65dc1cde4e80a49938f7d9b351a4049d5cf3
Kayden Lee <kayden@programmer.net>
2023-04-03 16:17:27 +0800

    added theme

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,22 @@ import "./index.css";
 import { router } from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { RouterProvider } from "react-router-dom";
-import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#795548",
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+  <ThemeProvider theme={theme}>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </ThemeProvider>
 );
 
 reportWebVitals();
