9e626f3a88b6bca7cf4b20da2cc7456e9452043e
Kayden Lee <kayden@programmer.net>
2023-04-03 16:56:57 +0800

    add navbar to order component

--- src/components/common/Order.tsx
+++ src/components/common/Order.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import NavBar from "./NavBar";
 
 function Order() {
   return (
-    <div>Order</div>
-  )
+    <div>
+      <NavBar />
+    </div>
+  );
 }
 
-export default Order
+export default Order;
