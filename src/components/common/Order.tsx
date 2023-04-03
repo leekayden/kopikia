9206fb9e75ad6d3803522b4bfe36e988122a8c59
Kayden Lee <kayden@programmer.net>
2023-04-03 17:04:18 +0800

    add order btn

--- src/components/common/Order.tsx
+++ src/components/common/Order.tsx
@@ -1,8 +1,9 @@
+import NavBar from "./NavBar";
 import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
-import NavBar from "./NavBar";
+import Button from '@mui/material/Button';
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
@@ -20,7 +21,7 @@ function Order() {
       <Box sx={{ flexGrow: 1 }}>
         <Grid container spacing={2}>
           <Grid item xs={8}>
-            <Item>xs=8</Item>
+            <Item><Button variant="contained" size="large">Take an order</Button></Item>
           </Grid>
           <Grid item xs={4}>
             <Item>xs=4</Item>
