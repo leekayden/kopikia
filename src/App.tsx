2b95a6e60e553a1b425bbb50e01329ba2311fb17
Kayden Lee <kayden@programmer.net>
2023-04-03 16:56:17 +0800

    add order page

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,16 @@ import "./App.css";
 import { createBrowserRouter } from "react-router-dom";
 import Error from "./components/common/Error";
 import Home from "./components/common/Home";
+import Order from "./components/common/Order";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
+  },
+  {
+    path: "/order",
+    element: <Order />,
   },
   {
     path: "*",
