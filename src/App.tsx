a10fb4d007d9b594dbae0b6f1a789ed8f4a564e8
Kayden Lee <kayden@programmer.net>
2023-04-03 16:32:17 +0800

    make not default export

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import "./App.css";
 import { createBrowserRouter } from "react-router-dom";
 import Error from "./components/common/Error";
 import Home from "./components/common/Home";
-import theme from "./index"
+import { theme } from "./index"
 
 export const router = createBrowserRouter([
   {
