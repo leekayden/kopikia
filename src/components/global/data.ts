9998a1f15088c8517fdcef2cf9b3b4bcee27ca9d
leekayden <kayden@programmer.net>
2023-04-04 19:10:29 +0800

    edit dummy data

--- src/components/global/data.ts
+++ src/components/global/data.ts
@@ -2,6 +2,7 @@ export const isLoggedIn = true;
 export const fName = "Adolf";
 export const lName = "Hitler";
 export const initials = fName.substring(0, 1) + lName.substring(0, 1);
+export const ownerId = 0;
 
 type OrderListType = {
   id: number;
@@ -11,5 +12,5 @@ type OrderListType = {
 
 export let OrderList: OrderListType[] = [
   { id: 0, ownerId: 0, name: "test order" },
-  { id: 0, ownerId: 0, name: "test order 2" },
+  { id: 1, ownerId: 0, name: "test order 2" },
 ];
