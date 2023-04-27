d106e6be2736b64a3930dc1ffcf98419854db7f6
Kayden Lee <kayden@programmer.net>
2023-04-28 00:04:29 +0800

    add dark mode

--- src/index.tsx
+++ src/index.tsx
@@ -5,9 +5,12 @@ import { router } from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { RouterProvider } from "react-router-dom";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import CssBaseline from "@mui/material/CssBaseline";
 
 export const theme = createTheme({
   palette: {
+    mode: "dark",
     primary: {
       main: "#795548",
     },
@@ -17,6 +20,7 @@ export const theme = createTheme({
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <CssBaseline />
       <RouterProvider router={router} />
     </ThemeProvider>
   </React.StrictMode>
