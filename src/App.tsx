a612012e10a6e5cd4437023db725e97fd57e8503
Kayden Lee <kayden@programmer.net>
2023-04-28 00:17:32 +0800

    add dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import React from "react";
 import "./App.css";
 import { createBrowserRouter } from "react-router-dom";
 import Error from "./components/common/Error";
 import Home from "./components/common/Home";
 import Order from "./components/common/Order";
 import TestServer from "./components/TestServer";
+import { RouterProvider } from "react-router-dom";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import CssBaseline from "@mui/material/CssBaseline";
 
 export const router = createBrowserRouter([
   {
@@ -23,3 +28,26 @@ export const router = createBrowserRouter([
     element: <Error errorCode={404} />,
   },
 ]);
+
+export default function App() {
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: prefersDarkMode ? "dark" : "light",
+          primary: {
+            main: "#795548",
+          },
+        },
+      }),
+    [prefersDarkMode]
+  );
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,28 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { router } from "./App";
+import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { RouterProvider } from "react-router-dom";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
-import useMediaQuery from "@mui/material/useMediaQuery";
-import CssBaseline from "@mui/material/CssBaseline";
-
-export const theme = createTheme({
-  palette: {
-    mode: "dark",
-    primary: {
-      main: "#795548",
-    },
-  },
-});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <RouterProvider router={router} />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>
 );
 
