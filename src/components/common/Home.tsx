84a46d90a72b5c058dcd2c796ecec02f195b0fb3
Kayden Lee <kayden@programmer.net>
2023-04-29 09:48:55 +0800

    removed unused

--- src/components/common/Home.tsx
+++ src/components/common/Home.tsx
@@ -5,27 +5,27 @@ let images = [
   {
     url: "/order",
     title: "Take Orders",
-    width: "25%",
+    width: "100%",
     bgUrl: "",
   },
-  {
-    url: "",
-    title: "Use previous orders",
-    width: "25%",
-    bgUrl: "",
-  },
-  {
-    url: "",
-    title: "Find good deals",
-    width: "25%",
-    bgUrl: "",
-  },
-  {
-    url: "",
-    title: "Take orders with friends",
-    width: "25%",
-    bgUrl: "",
-  },
+  // {
+  //   url: "",
+  //   title: "Use previous orders",
+  //   width: "25%",
+  //   bgUrl: "",
+  // },
+  // {
+  //   url: "",
+  //   title: "Find good deals",
+  //   width: "25%",
+  //   bgUrl: "",
+  // },
+  // {
+  //   url: "",
+  //   title: "Take orders with friends",
+  //   width: "25%",
+  //   bgUrl: "",
+  // },
 ];
 
 function Home() {
