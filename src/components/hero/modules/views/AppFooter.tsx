cc8d58cc389494e6c3a2f2d37a4862bc4a8f9d51
Kayden Lee <kayden@programmer.net>
2023-04-30 21:57:24 +0800

    update text

--- src/components/hero/modules/views/AppFooter.tsx
+++ src/components/hero/modules/views/AppFooter.tsx
@@ -1,19 +1,19 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import Link from '@mui/material/Link';
-import Container from '@mui/material/Container';
-import Typography from '../components/Typography';
-import TextField from '../components/TextField';
-import { AppName } from '../../../global/definitions';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Link from "@mui/material/Link";
+import Container from "@mui/material/Container";
+import Typography from "../components/Typography";
+import TextField from "../components/TextField";
+import { AppName } from "../../../global/definitions";
 
 function Copyright() {
   return (
     <React.Fragment>
-      {'© '}
+      {"© "}
       <Link color="inherit" href="/">
         {AppName}
-      </Link>{' '}
+      </Link>{" "}
       {new Date().getFullYear()}
     </React.Fragment>
   );
@@ -22,20 +22,20 @@ function Copyright() {
 const iconStyle = {
   width: 48,
   height: 48,
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  backgroundColor: 'warning.main',
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  backgroundColor: "warning.main",
   mr: 1,
-  '&:hover': {
-    bgcolor: 'warning.dark',
+  "&:hover": {
+    bgcolor: "warning.dark",
   },
 };
 
 const LANGUAGES = [
   {
-    code: 'en-US',
-    name: 'English',
+    code: "en-US",
+    name: "English",
   },
   // {
   //   code: 'fr-FR',
@@ -47,9 +47,9 @@ export default function AppFooter() {
   return (
     <Typography
       component="footer"
-      sx={{ display: 'flex', bgcolor: 'secondary.light' }}
+      sx={{ display: "flex", bgcolor: "secondary.light" }}
     >
-      <Container sx={{ my: 8, display: 'flex' }}>
+      <Container sx={{ my: 8, display: "flex" }}>
         <Grid container spacing={5}>
           <Grid item xs={6} sm={4} md={3}>
             <Grid
@@ -82,7 +82,7 @@ export default function AppFooter() {
             <Typography variant="h6" marked="left" gutterBottom>
               Legal
             </Typography>
-            <Box component="ul" sx={{ m: 0, listStyle: 'none', p: 0 }}>
+            <Box component="ul" sx={{ m: 0, listStyle: "none", p: 0 }}>
               <Box component="li" sx={{ py: 0.5 }}>
                 <Link href="/premium-themes/onepirate/terms/">Terms</Link>
               </Box>
@@ -113,11 +113,15 @@ export default function AppFooter() {
           </Grid>
           <Grid item>
             <Typography variant="caption">
-              {'Images from '}
-              <Link href="https://unsplash.com" rel="sponsored" title="Unsplash">
+              {"Images from "}
+              <Link
+                href="https://unsplash.com"
+                rel="sponsored"
+                title="Unsplash"
+              >
                 Unsplash
               </Link>
-              {', licensed by the '}
+              {", licensed by the "}
               <Link
                 href="https://unsplash.com/license/"
                 title="Unsplash License"
@@ -126,6 +130,7 @@ export default function AppFooter() {
               >
                 Unsplash License
               </Link>
+              {"."}
             </Typography>
           </Grid>
         </Grid>
