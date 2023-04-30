c775dd6d8d7cdef9f264e95e3a3be083224185aa
Kayden Lee <kayden@programmer.net>
2023-04-30 20:24:21 +0800

    remove unused

--- src/components/common/Home.tsx
+++ src/components/common/Home.tsx
@@ -1,16 +1,5 @@
-import NavBar from "./NavBar";
-import ImgBtn from "../components/ImgBtn";
-import Hero from "./Hero.old";
 import Index from "../hero/Home";
 
-let images = [
-  {
-    url: "/order",
-    title: "Take Orders",
-    width: "100%",
-    bgUrl: "",
-  },
-];
 
 function Home() {
   return (
